0524cab6633382412f9da51669ab74956f592130
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-02 10:25:03 -0400

    adding ingredients to recipe details

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,19 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
     recipes: Recipe[] = [
         new Recipe(
-          'A Test Recipe',
-          'This is a test recipe',
-          'https://cookieandkate.com/images/2018/12/pomegranate-pear-arugula-salad-recipe-2.jpg'
-        ),
+          'Tasty Schnitzel',
+          'A super-tasty schnitzel - just awesome',
+          'https://tse1.mm.bing.net/th?id=OIP.Uf7IpniAaFkXuHuxxcR5mgHaGi&pid=Api&P=0&w=176&h=157',
+        [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
         new Recipe(
-          'Another Test Recipe',
-          'This is a test recipe',
-          'https://cookieandkate.com/images/2018/12/pomegranate-pear-arugula-salad-recipe-2.jpg'
-        ),
+          'Big Fat Burger',
+          'What else you need to say?',
+          'https://tse4.mm.bing.net/th?id=OIP.Fofqzr_KgS6BCRJHgVw-cgHaJ4&pid=Api&P=0&w=300&h=300',
+        [ new Ingredient('Buns', 2), new Ingredient('Meat', 1)]),
       ];
 
     selectedRecipe = new EventEmitter<Recipe>();
