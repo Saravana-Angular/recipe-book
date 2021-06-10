6a8f54a290d8c34e66e93000106b3b5781d8986f
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-10 19:21:54 -0400

    retrieving router parameters

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -18,8 +18,8 @@ export class RecipeDetailComponent implements OnInit {
   ngOnInit(){
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = params['id'];
-        this.recipe = this.recipeService.getRecipe(+params['id']);
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id);
 
       }
     )
