864a2139475d998e9ab7a7c80304aad0b7092dc8
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-10 18:30:45 -0400

    implementing recipe-start component for the home page of recipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,10 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeDetailComponent, RecipeListComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent, DropdownDirective],
+  declarations: [AppComponent, HeaderComponent, RecipesComponent, RecipeDetailComponent, RecipeListComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent, DropdownDirective, RecipeStartComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
