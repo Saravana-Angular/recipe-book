6e684d75e514e11408ffa9363edb97ca6e0c436d
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-24 12:52:13 -0400

    fixing a bug with recipe resolver to retain temporary changes

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 import { DataStorageSerice } from "../shared/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class RecipesResolverService {
     
-    constructor(private dataStorageService: DataStorageSerice) {}
+    constructor(private dataStorageService: DataStorageSerice, private recipeService: RecipeService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataStorageService.fetchRecipes();
+        const recipes = this.recipeService.getRecipes();
+        if(recipes.length === 0) {
+            return this.dataStorageService.fetchRecipes();
+        }
+        return recipes;
     }
 
 }
