336797405ce52b1866d64b5d9d7a7b27e5736987
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-25 12:04:52 -0400

    adding loading spinner and handling error

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
+import { AuthService } from "./auth.service";
+
 @Component({
     selector: 'app-auth',
     templateUrl: './auth.component.html'
@@ -8,13 +10,33 @@ import { NgForm } from "@angular/forms";
 export class AuthComponent {
 
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+
+    constructor(private authService: AuthService) {}
+
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        const email = form.value.email;
+        const password = form.value.password;
+
+        this.isLoading = true;
+        if(this.isLoginMode) {
+            //..Login functionality
+        } else {
+            this.authService.signup(email, password).subscribe(resData => {
+                console.log(resData);
+                this.isLoading = false;
+            }, error => {
+                this.isLoading = false;
+                this.error = "An error occurred!";
+                console.log(error);
+            })
+        }
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -15,8 +15,8 @@ export class AuthService {
     constructor(private http: HttpClient) {}
 
     signup(email: string, password: string) {
-        this.http.post<AuthResponse>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=?key=AIzaSyDl_KQczWqWehdtFZf16-glHM3NfM4bm2s',
+        return this.http.post<AuthResponse>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDl_KQczWqWehdtFZf16-glHM3NfM4bm2s',
             {
                 email: email,
                 password: password,
