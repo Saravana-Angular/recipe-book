5434a79883e7a2d77d4a8207f6ded9d85e15fbe1
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-26 10:10:37 -0400

    sending login requests and handling login errors

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Observable } from "rxjs";
 
-import { AuthService } from "./auth.service";
+import { AuthResponse, AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -24,19 +25,25 @@ export class AuthComponent {
         const email = form.value.email;
         const password = form.value.password;
 
+        let authObs: Observable<AuthResponse>;
+
         this.isLoading = true;
+        this.error = null;
+        
         if(this.isLoginMode) {
-            //..Login functionality
+            authObs = this.authService.login(email, password);
         } else {
-            this.authService.signup(email, password).subscribe(resData => {
-                console.log(resData);
-                this.isLoading = false;
-            }, errorMessage => {
-                this.error = errorMessage;
-                console.log(errorMessage);
-                this.isLoading = false;
-            })
+            authObs = this.authService.signup(email, password);
         }
+        authObs.subscribe(resData => {
+            console.log(resData);
+            this.isLoading = false;
+        }, errorMessage => {
+            this.error = errorMessage;
+            console.log(errorMessage);
+            this.isLoading = false;
+        })
+        
         form.reset();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,15 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
 
-interface AuthResponse {
+export interface AuthResponse {
     idToken: string,
     email: string,
     refreshToken: string,
     expiresIn: string,
-    localId: string
+    localId: string,
+    registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -23,19 +24,37 @@ export class AuthService {
                 email: email,
                 password: password,
                 returnSecureToken: true
+            })
+            .pipe(catchError(this.handleError))
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponse>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDl_KQczWqWehdtFZf16-glHM3NfM4bm2s',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            })
+            .pipe(catchError(this.handleError))
+    }
+
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = "An unknown error has occurred!";
+        if(errorRes.error && errorRes.error.error) {
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                    errorMessage = "This email already exists!"
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = "This email does not exist!"
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = "The password is not correct!"
+                    break;
             }
-            ).pipe(
-                catchError((errorRes) => {
-                    let errorMessage = "An unknown error has occurred!";
-                    if(errorRes.error && errorRes.error.error) {
-                        switch (errorRes.error.error.message) {
-                            case 'EMAIL_EXISTS':
-                                errorMessage = "This email already exists!"
-                        }
-                    }
-                    return throwError(errorMessage);
-                }) 
-            )
+        }
+        return throwError(errorMessage);
     }
 
 }
