1f834c7a385edfd698109135ad5eadebbbd4f327
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-29 12:30:54 -0400

    clearing only the userData instead of entire localStorage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -67,7 +67,7 @@ export class AuthService {
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
-        localStorage.clear();
+        localStorage.removeItem('userData');
     }
 
 
