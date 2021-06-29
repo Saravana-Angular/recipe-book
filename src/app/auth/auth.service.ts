a73e1360b59f632e863c11fbebf78f96649017df
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-29 12:18:21 -0400

    adding autologin for localstorage of user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -47,15 +47,33 @@ export class AuthService {
             }))
     }
 
+    autoLogin() {
+        const userData: {
+            email: string,
+            id: string,
+            token: string,
+            tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData) {
+            return;
+        }
+
+        const loadedUser = new User(userData.email, userData.id, userData.token, new Date(userData.tokenExpirationDate));
+        this.user.next(loadedUser);
+    }
+
     logout() {
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.clear();
     }
+
 
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
