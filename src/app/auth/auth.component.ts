a5618c2b893611f3bdf587a497b24cf982bb2707
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-30 10:29:47 -0400

    creating component dynamically

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
-import { Component, ComponentFactoryResolver } from "@angular/core";
+import { Component, ComponentFactoryResolver, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AlertComponent } from "../shared/alert/alert.component";
+import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 
 import { AuthResponse, AuthService } from "./auth.service";
 
@@ -15,6 +16,7 @@ export class AuthComponent {
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+    @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
 
     constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver) {}
 
@@ -58,5 +60,8 @@ export class AuthComponent {
 
     private showErrorAlert(message: string) {
         const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear();
+        hostViewContainerRef.createComponent(alertCmpFactory);
     }
 }
