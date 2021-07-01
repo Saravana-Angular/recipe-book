2dccca47bada974431328ce276587de21f719b5a
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-06-30 22:44:28 -0400

    lazy loading auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { AuthModule } from './auth/auth.module';
     BrowserModule, 
     AppRoutingModule, 
     HttpClientModule,
-    AuthModule,
     SharedModule,
     CoreModule
   ],
