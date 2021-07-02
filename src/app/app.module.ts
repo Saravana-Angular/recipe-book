e67857fb584df850630e15194744cffbda6941f8
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-02 16:58:37 -0400

    accessing state from shopping list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Observable, Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
 
 import { Ingredient } from '../shared/ingredient.model';
@@ -11,15 +12,20 @@ import { ShoppingListService } from './shopping-list.service';
   styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
-  ingredients: Ingredient[]; 
+  ingredients: Observable<{ ingredients: Ingredient []}>; 
   private subscription: Subscription;
-  constructor(private slService: ShoppingListService, private loggingService: LoggingService) {}
+  constructor(
+    private slService: ShoppingListService, 
+    private loggingService: LoggingService,
+    private store: Store<{ shoppingList: { ingredients: Ingredient[]}}>
+    ) {}
 
   ngOnInit() {
-    this.ingredients = this.slService.getIngredients();
-    this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient []) => {
-      this.ingredients = ingredients;
-    })
+    this.ingredients = this.store.select('shoppingList');
+    // this.ingredients = this.slService.getIngredients();
+    // this.subscription = this.slService.ingredientsChanged.subscribe((ingredients: Ingredient []) => {
+    //   this.ingredients = ingredients;
+    // })
     this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit');
   }
 
@@ -28,7 +34,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
   }
 
 }
