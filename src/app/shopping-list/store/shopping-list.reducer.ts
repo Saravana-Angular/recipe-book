12fb9ee208a122fc0afc1bbc45e07ac8075f2915
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-02 16:03:40 -0400

    adding actions for addingredient and updating reducer

---
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,19 @@
+import { Ingredient } from "../../shared/ingredient.model";
+import { ADD_INGREDIENT } from "./shopping-list.actions";
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ]
+}
+
+export function shoppingListReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      }
+  }
+}
