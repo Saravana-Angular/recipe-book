6a8824a6094c89d958c6206493216bc5636e11e4
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-02 16:13:25 -0400

    providing store in the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
-import { LoggingService } from './logging.service';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { LoggingService } from './logging.service';
     BrowserModule, 
     AppRoutingModule, 
     HttpClientModule,
+    StoreModule.forRoot({ shoppingList: shoppingListReducer}),
     SharedModule,
     CoreModule
   ],
