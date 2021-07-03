f09ed097edcda97b5f114ab736ace4e83b2b46e7
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-02 21:38:46 -0400

    preparing  update and delete actions for shoppinglist

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -1,9 +1,12 @@
+import { Action } from "@ngrx/store";
 import { Ingredient } from "src/app/shared/ingredient.model";
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
 
-export class AddIngredient {
+export class AddIngredient implements Action {
 
     readonly type = ADD_INGREDIENT;
     // payload: Ingredient
@@ -11,10 +14,22 @@ export class AddIngredient {
 
 }
 
-export class AddIngredients {
+export class AddIngredients implements Action {
  
     readonly type = ADD_INGREDIENTS;
     constructor(public payload: Ingredient[]) {}
 }
 
-export type ShoppingListActions = AddIngredient | AddIngredients;
+export class UpdateIngredient implements Action {
+
+    readonly type = UPDATE_INGREDIENT;
+    constructor(public payload: {id: number, ingredient: Ingredient}) {}
+}
+
+export class DeleteIngredient implements Action {
+
+    readonly type = DELETE_INGREDIENT;
+    constructor(public payload: number) {}
+}
+
+export type ShoppingListActions = AddIngredient | AddIngredients | UpdateIngredient | DeleteIngredient;
