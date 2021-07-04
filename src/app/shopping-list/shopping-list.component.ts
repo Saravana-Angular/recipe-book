2e8570464e83b395d0713f07bbfd1960b196e6b2
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-04 09:32:26 -0400

    removing shopping-list service since it was replaced by ngrs store

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,7 +4,6 @@ import { Observable, Subscription } from 'rxjs';
 import { LoggingService } from '../logging.service';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import * as fromShoppingList from './store/shopping-list.reducer';
 import * as ShoppingListActions from './store/shopping-list.actions';
 
@@ -16,8 +15,7 @@ import * as ShoppingListActions from './store/shopping-list.actions';
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Observable<{ ingredients: Ingredient []}>; 
   private subscription: Subscription;
-  constructor(
-    private slService: ShoppingListService, 
+  constructor( 
     private loggingService: LoggingService,
     private store: Store<fromShoppingList.AppState>
     ) {}
