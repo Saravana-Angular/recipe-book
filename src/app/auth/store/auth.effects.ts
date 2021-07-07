b6019cc89e243013cb5354dc6439a3d2d4daf2fa
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-06 21:51:19 -0400

    preparing other auth actions

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -21,6 +21,11 @@ export class AuthEffects {
     
     constructor(private actions$: Actions, private http: HttpClient, private router: Router) {}
 
+    authSignup = this.actions$.pipe(
+        ofType(AuthActions.SIGNUP_START),
+        tap()
+    )
+
     @Effect()
     authLogin = this.actions$.pipe(
             ofType(AuthActions.LOGIN_START),
@@ -35,7 +40,7 @@ export class AuthEffects {
                         map(resData => {
                             const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000)
                             
-                            return (new AuthActions.Login({
+                            return (new AuthActions.AuthenticateSuccess({
                                     email: resData.email,
                                     userId: resData.localId,
                                     token: resData.idToken,
@@ -44,7 +49,7 @@ export class AuthEffects {
                         }), catchError(errorRes => {
                             let errorMessage = "An unknown error has occurred!";
                             if(!errorRes.error || !errorRes.error.error) {
-                                return of (new AuthActions.LoginFail(errorMessage));
+                                return of (new AuthActions.AuthenticateFail(errorMessage));
                             }
                             
                             switch (errorRes.error.error.message) {
@@ -59,13 +64,13 @@ export class AuthEffects {
                                     break;
                             }
                             
-                            return of(new AuthActions.LoginFail(errorMessage));
+                            return of(new AuthActions.AuthenticateFail(errorMessage));
             }))
                 
             }))
  
     @Effect({dispatch: false})      
-    authSuccess = this.actions$.pipe(ofType(AuthActions.LOGIN), tap(() => {
+    authSuccess = this.actions$.pipe(ofType(AuthActions.AUTHENTICATE_SUCCESS), tap(() => {
         this.router.navigate(['/'])
     }))
 }
--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -17,7 +17,7 @@ export function authReducer(state = initialState, action: AuthActions.AuthAction
 
     switch (action.type) {
 
-        case AuthActions.LOGIN:
+        case AuthActions.AUTHENTICATE_SUCCESS:
             const user = new User(
                 action.payload.email,
                 action.payload.userId,
@@ -38,7 +38,7 @@ export function authReducer(state = initialState, action: AuthActions.AuthAction
                 loading: true
             }
             
-        case AuthActions.LOGIN_FAIL:
+        case AuthActions.AUTHENTICATE_FAIL:
             return {
                 ...state,
                 user: null,
