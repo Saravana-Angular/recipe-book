11538786c09e7b1595115b499d2fdf429d7a76f3
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-08 11:43:58 -0400

    implementing setRecipes action

--- src/app/recipes/store/recipe.reducer.ts
+++ src/app/recipes/store/recipe.reducer.ts
@@ -1,4 +1,5 @@
 import { Recipe } from "../recipe.model";
+import * as RecipesActions from './recipe.actions';
 
 export interface State {
     recipes: Recipe[];
@@ -7,6 +8,14 @@ export interface State {
 const initialState: State = {
     recipes: []
 }
-export function recipeReducer(state = initialState, action) {
-    return state;
+export function recipeReducer(state = initialState, action: RecipesActions.RecipesActions) {
+    switch(action.type) {
+        case RecipesActions.SET_RECIPES:
+            return {
+                ...state,
+                recipes: [...action.payload]
+            }
+        default:
+            return state;
+    }
 }
