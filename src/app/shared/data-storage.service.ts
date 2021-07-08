38bdaae3e63d7d1a150d7bb22257fb64deee6101
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2021-07-08 12:19:23 -0400

    dispatching setRecipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,10 +4,17 @@ import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from '../recipes/recipe.model';
 import { AuthService } from "../auth/auth.service";
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+import { Store } from "@ngrx/store";
 
 @Injectable({providedIn: 'root'})
 export class DataStorageSerice {
-    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+    constructor(
+        private http: HttpClient, 
+        private recipeService: RecipeService, 
+        private authService: AuthService,
+        private store: Store<fromApp.AppState>) {}
     
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -26,7 +33,8 @@ export class DataStorageSerice {
                 return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
             })
         }),tap(recipes => {
-            this.recipeService.setRecipes(recipes);
+            // this.recipeService.setRecipes(recipes);
+            this.store.dispatch(new RecipesActions.SetRecipes(recipes));
         }));
     }
 }
